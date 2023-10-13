e32d64be8113eb37d164af8ce489708c5605c654
tzdeb <t.z.t.968@gmail.com>
2023-10-13 20:36:19 +0200

    Move controller to the dedicated package
    Signed-off-by: tzdeb <t.z.t.968@gmail.com>

--- Backend/swims/src/main/java/com/tzc/swims/controllers/HelloWorldController.java
+++ Backend/swims/src/main/java/com/tzc/swims/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.tzc.swims;
+package com.tzc.swims.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
