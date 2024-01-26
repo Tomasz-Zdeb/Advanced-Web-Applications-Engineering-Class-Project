b4b1a3d6f22e96af58f78c5ec51867228e56f2b8
Tomasz Zdeb <62356875+Tomasz-Zdeb@users.noreply.github.com>
2024-01-26 12:39:36 +0100

    Add png image endpoints for delete and create

--- Backend/swims/src/main/java/com/tzcustom/swims/controller/ImageController.java
+++ Backend/swims/src/main/java/com/tzcustom/swims/controller/ImageController.java
@@ -2,12 +2,15 @@ package com.tzcustom.swims.controller;
 
 import com.tzcustom.swims.model.PngImageModel;
 import com.tzcustom.swims.repository.PngImageRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -26,6 +29,32 @@ public class ImageController {
         Optional<PngImageModel> returnedImage = pngImageRepository.findById(uuid);
         return returnedImage.map(pngImageModel -> ResponseEntity
                 .ok(pngImageModel.getImageBinary()))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //@Operation(summary = "Upload a PNG image", description = "Uploads a new PNG image to the server")
+    @PostMapping(path = "/upload/image/png", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadPngImage(@RequestParam("file") MultipartFile file){
+        try {
+            PngImageModel pngImage = new PngImageModel();
+            pngImage.setImageBinary(file.getBytes());
+            pngImage.setId(UUID.randomUUID());
+            pngImage = pngImageRepository.save(pngImage);
+
+            UUID imageId = pngImage.getId();
+            return ResponseEntity.ok().body("Image uploaded successfully with ID:" + imageId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Could not upload the image: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping(value = "/delete/image/png/{uuid}")
+    public ResponseEntity<?> deletePngImage(@PathVariable UUID uuid) {
+        return pngImageRepository.findById(uuid)
+                .map(pngImageModel -> {
+                    pngImageRepository.delete(pngImageModel);
+                    return ResponseEntity.ok().body("Image with ID " + uuid + " was deleted successfully.");
+                })
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- Backend/swims/src/main/java/com/tzcustom/swims/model/PngImageModel.java
+++ Backend/swims/src/main/java/com/tzcustom/swims/model/PngImageModel.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "png_images")
 public class PngImageModel {
     @Id
