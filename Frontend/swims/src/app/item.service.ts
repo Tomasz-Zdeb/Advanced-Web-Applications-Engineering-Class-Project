2c8892097ffdb48e489af6945d1897b66827d836
Tomasz Zdeb <62356875+Tomasz-Zdeb@users.noreply.github.com>
2024-01-27 13:21:09 +0100

    Implement item display

---
+++ Frontend/swims/src/app/item.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Item } from './item.interface'; // Import the Item interface
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  private baseUrl = 'http://localhost:8080/api/items';
+
+  constructor(private http: HttpClient) { }
+
+  getByStorageSpaceName(storageSpaceName: string): Observable<Item[]> {
+    const url = `${this.baseUrl}/by-storage-space?storage_space_name=${storageSpaceName}`;
+    return this.http.get<Item[]>(url);
+  }
+}
--- Frontend/swims/src/app/storage-space-details/storage-space-details.component.ts
+++ Frontend/swims/src/app/storage-space-details/storage-space-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { StorageSpace } from '../storage.space.interface';
 import { StorageSpaceService } from '../storage.space.service';
 import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../item.service';
+import { Item } from '../item.interface';
 
 @Component({
   selector: 'swims-storage-space-details',
@@ -11,26 +13,58 @@ import { ActivatedRoute } from '@angular/router';
 export class StorageSpaceDetailsComponent implements OnInit{
   storageSpace?: StorageSpace;
   searchName: string = '';
+  items: Item[] = [];
+  public myStyles: any;
 
-  constructor(private storageSpaceService: StorageSpaceService, private route: ActivatedRoute){
+  constructor(private storageSpaceService: StorageSpaceService, private route: ActivatedRoute, private itemService: ItemService){
+    this.updateStyles(window.innerWidth);
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateStyles((event.target as Window).innerWidth);
+  }
+
+  private updateStyles(width: number) {
+    if (width > 1200) {
+      this.myStyles = {
+        'max-height': '90vh',
+        'overflow-y': 'scroll'
+      };
+    } else {
+      this.myStyles = {
+        'max-height': '100%',
+        'overflow-y': 'auto'
+      };
+    }
   }
 
   ngOnInit(): void {
+    // Fetches storage space name from the route
     this.route.paramMap.subscribe(params => {
-      const param = params.get('name'); // Replace 'paramName' with the name of your route parameter
+      const param = params.get('name');
       if (param) {
           this.searchName = param;
-          console.log(`Parameter Value: ${param}`);
       }
   });
+  // Fetches data using storage space name
   this.fetchData();
   }
 
   fetchData() {
+    // Fetch storage space details
     this.storageSpaceService.fetchStorageSpaces().subscribe(
       data => this.storageSpace = data.find(space => space.name === this.searchName),
       error => console.error('Error:', error)
+    );
+    // Fetch Items
+    this.itemService.getByStorageSpaceName('main-warehouse').subscribe(
+      data => {
+        this.items = data;
+      },
+      error => {
+        console.error('There was an error!', error);
+      }
     );
   }
 }
